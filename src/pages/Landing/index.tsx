f01be31a01ec2187d71aeadb36b18cf429f5ba33
Isaac Alencar <alencarisaac3@gmail.com>
2020-08-04 11:14:26 -0300

    update Landing page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import {
   PageLanding,
@@ -25,15 +26,15 @@ const Landing: React.FC = () => {
         <img src={landingImg} alt="landing-imgage" className="hero-image" />
 
         <ButtonsContainer>
-          <a href="/" className="study">
+          <Link to="/study" className="study">
             <img src={studyIcon} alt="Estudar" />
             Estudar
-          </a>
+          </Link>
 
-          <a href="/" className="give-classes">
+          <Link to="/give-leassons" className="give-classes">
             <img src={giveClassesIcon} alt="Dar aulas" />
             Dar Aulas
-          </a>
+          </Link>
         </ButtonsContainer>
         <span className="total-connections">
           Total de 200 conexões já realizadas
